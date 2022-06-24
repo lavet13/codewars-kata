c033c48f189a0d50c7b8735704ec24eff07ca191
lavet13 <lavet13@mail.ru>
2022-06-24 17:57:12 +0300

    unfinished yet, but close to solve tbh

--- Where is my parent_cry/script.js
+++ Where is my parent_cry/script.js
@@ -18,49 +18,87 @@ function findChildren(dancingBrigade) {
     return false;
   };
 
-  if (
-    !/[0-9]/g.test(dancingBrigade) &&
-    !hasDuplicates(dancingBrigade.split(''))
-  ) {
-    const findUpperCaseLetters = function (string) {
-      const result = [];
-      let index = 0;
-      while (index !== -1) {
-        index = string.search(/[A-Z]/);
-        if (index !== -1) {
-          result.push(string[index]);
-          string = string.slice(index + 1);
-        }
+  // const findUpperCaseLetters = function (array) {
+  //   const result = [];
+  //   let index = 0;
+
+  //   while (index !== -1) {
+  //     index = array.join('').search(/[A-Z]/);
+
+  //     if (index !== -1) {
+  //       result.push(array[index]);
+  //       array = array.slice(index + 1);
+  //     }
+  //   }
+
+  //   return result.sort((a, b) => {
+  //     if (a < b) {
+  //       return -1;
+  //     }
+  //     if (a > b) {
+  //       return 1;
+  //     }
+  //     return 0;
+  //   });
+  // };
+
+  const findUpperCaseLetters = function (array) {
+    let index;
+
+    while (index !== -1) {
+      index = array.join('').search(/[a-z]/);
+      if (index !== -1) {
+        array.splice(index, 1);
       }
+    }
 
-      return result;
-    };
+    return array.sort();
+  };
 
-    const findLowerCaseLetters = function (string) {
-      const result = [];
-      const counts = {};
-      // FIXME how many letters does this function find, in this case probably use object to collect the amount of those ones
-      for (let i = 0; i < string.length; i++) {
-        let letter = string[i];
+  const findLowerCaseLetters = function (array) {
+    const result = [],
+      counts = {};
+
+    let index = 0,
+      letter;
+
+    while (index !== -1) {
+      index = array.join('').search(/[a-z]/);
+
+      if (index !== -1) {
+        letter = array[index];
 
         if (letter in counts) {
           counts[letter] += 1;
         } else {
           counts[letter] = 1;
         }
+
+        result.push(array[index]);
+        array = array.slice(index + 1);
       }
+    }
 
-      return result;
-    };
+    return { counts, result };
+  };
 
-    const upperCases = findUpperCaseLetters(dancingBrigade);
-    const lowerCases = findLowerCaseLetters(dancingBrigade);
+  const concatLetters = function (upper, lower) {};
 
-    return '';
+  const upperCases = findUpperCaseLetters(dancingBrigade.split('')); // since it's modifying original array
+
+  if (!/[0-9]/g.test(dancingBrigade) && !hasDuplicates(upperCases)) {
+    console.log('пернул');
+
+    const lowerCases = findLowerCaseLetters(dancingBrigade.split(''));
+
+    concatLetters(upperCases, lowerCases);
+
+    console.log(upperCases, lowerCases);
   }
+  return '';
 }
 
-findChildren('aAbaBb');
+findChildren('beeeEBb');
 
 // https://stackoverflow.com/questions/7376598/in-javascript-how-do-i-check-if-an-array-has-duplicate-values
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/in
