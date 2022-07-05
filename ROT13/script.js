bcab6eb54e579306b4d06e804183ef8e1f8b62e0
lavet13 <lavet13@mail.ru>
2022-07-05 15:03:42 +0300

    fixed

--- ROT13/script.js
+++ ROT13/script.js
@@ -1,34 +1,44 @@
 'use strict';
 
-rot13('EBG13 rknzcyr.');
+rot13('rknzcyr.');
 
 function rot13(str) {
     substitueCharacters(str);
 }
 
 function substitueCharacters(str) {
-    const regex = RegExp('[a-m]', 'g');
-    let found;
+    const firstHalf = 'abcdefghijklm';
+    const secondHalf = 'nopqrstuvwxyz';
+    let index, letter;
 
-    while((found = regex.exec(str)) !== null) {
-        if(/[a-m]/g.test(found[0])) {
-            console.log(`Found ${found[0]}. Next starts at ${regex.lastIndex}.`)
-        } else {
+    findLetters();
 
+    
+
+    function findLetters() {
+        const array = {};
+
+        for(let symbol of str) {
+
+            if(/[a-m]/.test(symbol)) {
+                index = firstHalf.indexOf(symbol);
+                letter = secondHalf[index];
+                // console.log(letter);
+                // console.log(index);
+            } else if(/[n-z]/.test(symbol)) {
+                index = secondHalf.indexOf(symbol);
+                letter = firstHalf[index];
+                // console.log(letter);
+                // console.log(index);
+            }            
         }
-            
     }
-}
-
-
-function swapLetters() {
-    const firstHalf = "abcdefghijklm";
-    const secondHalf = "nopqrstuvwxyz";
-
 
 }
 
-swapLetters();
+
+
+
 
 
 // https://stackoverflow.com/questions/35613451/searching-a-string-for-missing-letters-of-the-alphabet-in-javascript
@@ -38,7 +48,6 @@ swapLetters();
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/lastIndex
 
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_function_as_a_parameter
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Quantifiers
 
