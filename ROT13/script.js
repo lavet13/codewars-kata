50f50df245de9130b230bca9cb2e05c4a822533e
lavet13 <lavet13@mail.ru>
2022-07-05 16:56:17 +0300

    worked

--- ROT13/script.js
+++ ROT13/script.js
@@ -1,40 +1,41 @@
 'use strict';
 
-rot13('rknzcyr.');
+console.log(rot13('This is my first ROT13 excercise!'));
 
 function rot13(str) {
-    substitueCharacters(str);
+    return substitueCharacters(str).join('');
 }
 
 function substitueCharacters(str) {
+
     const firstHalf = 'abcdefghijklm';
     const secondHalf = 'nopqrstuvwxyz';
-    let index, letter;
 
-    findLetters();
+    const isUpperCase = function (letter) {
+        return letter === letter.toUpperCase();
+    };
 
-    
+    const array = [];
+    let letter;
+    let index;
 
-    function findLetters() {
-        const array = {};
-
-        for(let symbol of str) {
-
-            if(/[a-m]/.test(symbol)) {
-                index = firstHalf.indexOf(symbol);
-                letter = secondHalf[index];
-                // console.log(letter);
-                // console.log(index);
-            } else if(/[n-z]/.test(symbol)) {
-                index = secondHalf.indexOf(symbol);
-                letter = firstHalf[index];
-                // console.log(letter);
-                // console.log(index);
-            }            
-        }
+    for(let symbol of str) {
+        if(/[a-m]/i.test(symbol)) {
+            index = firstHalf.indexOf(symbol.toLowerCase());
+            letter = secondHalf[index];
+            array.push(isUpperCase(symbol) ? letter.toUpperCase() : letter.toLowerCase());
+        } else if(/[n-z]/i.test(symbol)) {
+            index = secondHalf.indexOf(symbol.toLowerCase());
+            letter = firstHalf[index];
+            array.push(isUpperCase(symbol) ? letter.toUpperCase() : letter.toLowerCase());
+        } else {
+            array.push(symbol);
+        }       
     }
-
+    
+    return array;
 }
+
 
 
 
@@ -43,16 +44,6 @@ function substitueCharacters(str) {
 
 // https://stackoverflow.com/questions/35613451/searching-a-string-for-missing-letters-of-the-alphabet-in-javascript
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
+// https://stackoverflow.com/questions/3115982/how-to-check-if-two-arrays-are-equal-with-javascript
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/lastIndex
-
-
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Quantifiers
-
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Character_Classes
-
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/search
+// https://www.freecodecamp.org/news/how-to-clone-an-array-in-javascript-1d3183468f6a/
