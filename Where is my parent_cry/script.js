05648b175a40420185af07e95b5c29a2621a1879
lavet13 <lavet13@mail.ru>
2022-07-05 17:03:08 +0300

    codewars example

--- Where is my parent_cry/script.js
+++ Where is my parent_cry/script.js
@@ -109,6 +109,86 @@ function findChildren(dancingBrigade) {
 
 console.log(findChildren('beeeEBb'));
 
+
+
+
+
+/*
+// FIXME
+Codewars Example:
+
+function rot13(str) {
+ var code = {
+    A: "N",
+    B: "O",
+    C: "P",
+    D: "Q", 
+    E: "R",
+    F: "S",
+    G: "T",
+    H: "U",
+    I: "V",
+    J: "W",
+    K: "X",
+    L: "Y",
+    M: "Z",
+    N: "A",
+    O: "B",
+    P: "C",
+    Q: "D",
+    R: "E",
+    S: "F",
+    T: "G",
+    U: "H",
+    V: "I",
+    W: "J",
+    X: "K",
+    Y: "L",
+    Z: "M",
+    a: "n",
+    b: "o",
+    c: "p",
+    d: "q",
+    e: "r",
+    f: "s",
+    g: "t",
+    h: "u",
+    i: "v",
+    j: "w",
+    k: "x",
+    l: "y",
+    m: "z",
+    n: "a",
+    o: "b",
+    p: "c",
+    q: "d",
+    r: "e",
+    s: "f",
+    t: "g",
+    u: "h",
+    v: "i",
+    w: "j",
+    x: "k",
+    y: "l",
+    z: "m"
+  };
+  var splitStr = str.split("");
+  for (var i = 0; i < splitStr.length; i++){
+      for (var prop in code){
+          if (prop === splitStr[i]){
+              splitStr[i] = code[prop];
+             break;
+          }
+      }
+  }
+  var codeman = splitStr.join('');
+  return codeman;
+}
+
+*/
+
+
+
 // https://stackoverflow.com/questions/7376598/in-javascript-how-do-i-check-if-an-array-has-duplicate-values
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/in
