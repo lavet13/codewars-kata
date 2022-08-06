5a371ca53f8b54218c5a1a7e622aced405ef1d20
lavet13 <lavet13@mail.ru>
2022-08-06 03:06:09 +0300

    asdf

--- parseInt() reloaded/test.js
+++ parseInt() reloaded/test.js
@@ -1,30 +1,39 @@
 // let map = [7, 3, 4, 5];
 
-//// obj = Array.from(map).reduce(
-////   (acc, [key, val]) => Object.assign(acc, { [key]: val }),
-////   {}
-//// );
-
 // let obj = map.reduce((previousValue, currentValue, index) => {
 //   // previousValue, currentValue, index, array
 //   return Object.assign(previousValue, { [index]: currentValue }); // Enhanced object literal
 // }, {});
 
 let map = new Map().set('foo', true).set('bar', false);
+for (let element of map) {
+  // so again nothing special, so without values method which returns iterator object works as expected
+  console.log(element);
+}
+
 // map have keys, values methods
 let arr = Array.from(map); // similar what all i did back then is was Object.entries();
+console.log(arr);
+
 // let set = new Set(map); // entries are arrays which we don't want at all
 let set = new Set(map.values());
 
 // getting index along with key value pairs
-// for (let value of arr.entries()) {
-//   console.log(value);
-// }
-
-// getting key value pairs
-for (let value of arr.values()) {
+for (let value of arr.entries()) {
   console.log(value);
 }
+
+//console.log(arr.values()); // Array Iterator
+
+//getting key value pairs
+//for (let value of arr.values()) {
+//console.log(value);
+//}
+
+//getting key value pairs, similar as above
+//for (let value of arr) {
+//console.log(value);
+//}
 
 console.log(arr);
 console.log(set);
