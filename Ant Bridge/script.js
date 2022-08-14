48d683d1e5fbf5dd748d4331941477cd42b9b42b
lavet13 <lavet13@mail.ru>
2022-08-14 19:56:06 +0300

    js

--- Ant Bridge/script.js
+++ Ant Bridge/script.js
@@ -1,4 +1,24 @@
 'use strict';
 
-function antBridge(ants, terrain) {}
+function antBridge(ants, terrain) {
+  const getBridge = function (terrain) {
+    let bridgeArray = [];
+
+    while (terrain.indexOf('.') !== -1) {
+      bridgeArray.push(
+        terrain.slice(
+          terrain.indexOf('.'),
+          terrain.indexOf('-', terrain.indexOf('.'))
+        )
+      );
+
+      terrain = terrain.slice(terrain.indexOf('-', terrain.indexOf('.')));
+    }
+
+    return bridgeArray;
+  };
+
+  let bridgeArray = getBridge(terrain);
+  console.log(bridgeArray);
+}
 console.log(`result is ${antBridge('GFEDCBA', '----.-.---...----..-----')}`);
